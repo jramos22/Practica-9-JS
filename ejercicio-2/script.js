cd7a32c6e8e7ac41290050e3b58c097bac928f20
José Daniel Ramos Barquero <josedanielramosbarquero@gmail.com>
2020-08-07 14:28:19 -0600

    agrego el ejercicio 2 de js

---
+++ ejercicio-2/script.js
@@ -0,0 +1,28 @@
+function color(event) {
+  cambio(valor);
+  for (let j = 0; j < elements.length; j++) {
+    if (valor == true) {
+      event.currentTarget.style.backgroundColor = 'red';
+      valor = !valor;
+    } else {
+      event.currentTarget.style.backgroundColor = 'blue';
+      valor = !valor;
+    }
+  }
+}
+function cambio(valor) {
+  return valor;
+}
+
+// necesito un ciclo que me marque las clases 
+let ids = 0;
+let valor = true;
+let elements = 0;
+for (let ids = 0; ids < 9; ids++) {
+  elements = document.querySelectorAll(`#c${ids}`);
+  for (let i = 0; i < elements.length; i++) {
+    valor = true;
+    elements[i].addEventListener('click', color);
+  }
+
+}
